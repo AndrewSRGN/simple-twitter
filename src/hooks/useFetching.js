2fae0c6ca656480b4d4f6e4d267337d65df5875e
Andrei Siarheyenka <84916548+AndrewSRGN@users.noreply.github.com>
2024-05-18 19:47:09 -0700

    Refactor/Fix hook useFetching

--- src/hooks/useFetching.js
+++ src/hooks/useFetching.js
@@ -1,42 +1,26 @@
-import {useEffect, useState} from "react";
+import {useState} from "react";
 
 /**
- * Hook for executing multiple asynchronous operations with loading and error state management.
- * @param {Array<Function>} callbacks Array of callback functions, each of which should be asynchronous and сhange the state.
- * @returns {[boolean, Error | null]} Array of states: isLoading (loading) and error (error).
+ * Custom hook for executing multiple asynchronous operations with loading and error state management.
+ * @param {Array<Function>} callbacks Array of callback functions, each of which should be asynchronous and change the state.
+ * @returns {[fetchData:function, isLoading: boolean, error: string | null]}
  */
 
 export const useFetching = (callbacks) => {
     const [isLoading, setIsLoading] = useState(false);
     const [error, setError] = useState(null);
 
-    useEffect(() => {
-        let isMounted = true;
-
-        const fetching = async () => {
-            try {
-                setIsLoading(true);
-                await Promise.all (
-                    callbacks.map(callback => callback())
-                );
-            } catch (error) {
-                if (isMounted) {
-                    setError(error.message);
-                }
-            } finally {
-                if (isMounted) {
-                    setIsLoading(false);
-                }
-            }
+    const fetchData = async () => {
+        try {
+            setIsLoading(true)
+            await Promise.all(callbacks.map(callback => callback()));
+        } catch (error) {
+            setError(error.message);
+        } finally {
+            setIsLoading(false);
         }
+    }
 
-        fetching();
 
-        return () => {
-            isMounted = false;
-            callbacks.length = 0; //If component is unmounted, remove all callbacks
-        }
-    }, [callbacks]);
-
-    return [isLoading, error];
+    return [fetchData, isLoading, error];
 }
