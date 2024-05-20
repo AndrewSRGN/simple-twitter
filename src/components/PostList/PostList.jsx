ba8aa8678e9e2912ebc278a32de87db53ab19b63
Andrei Siarheyenka <84916548+AndrewSRGN@users.noreply.github.com>
2024-05-20 01:12:49 -0700

    Refactor component PostList

--- src/components/PostList/PostList.jsx
+++ src/components/PostList/PostList.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Post from "../Post/Post";
 import './PostList.css';
+import {CSSTransition, TransitionGroup} from "react-transition-group";
 
 const PostList = ({title, posts, removePost}) => {
     if (!posts.length) {
@@ -10,14 +11,22 @@ const PostList = ({title, posts, removePost}) => {
     return (
         <div className="post-list">
             <h1 className="post-list__title">{title}</h1>
-            {posts.map((post, index) => (
-                <Post
-                    remove={removePost}
-                    key={post.id}
-                    index={index + 1}
-                    post={post}
-                />
-            ))}
+            <TransitionGroup className="post-list__body">
+                {posts.map((post, index) => (
+                    <CSSTransition
+                        key={post.id}
+                        timeout={500}
+                        classNames="post"
+                    >
+                        <Post
+                            remove={removePost}
+                            key={post.id}
+                            index={index + 1}
+                            post={post}
+                        />
+                    </CSSTransition>
+                ))}
+            </TransitionGroup>
         </div>
     );
 };
