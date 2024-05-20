1c63eb3b3dd30a6f6a819a9da27fa6530c8354af
Andrei Siarheyenka <84916548+AndrewSRGN@users.noreply.github.com>
2024-05-20 11:36:29 -0700

    Add nodeRef for CSSTransition in component PostList

--- src/components/PostList/PostList.jsx
+++ src/components/PostList/PostList.jsx
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, {createRef} from 'react';
 import Post from "../Post/Post";
 import './PostList.css';
 import {CSSTransition, TransitionGroup} from "react-transition-group";
 
 const PostList = ({title, posts, removePost}) => {
+
+
     if (!posts.length) {
         return <h1 style={{ textAlign: "center" }}>Posts not found</h1>;
     }
@@ -12,20 +14,24 @@ const PostList = ({title, posts, removePost}) => {
         <div className="post-list">
             <h1 className="post-list__title">{title}</h1>
             <TransitionGroup className="post-list__body">
-                {posts.map((post, index) => (
-                    <CSSTransition
-                        key={post.id}
-                        timeout={500}
-                        classNames="post"
-                    >
-                        <Post
-                            remove={removePost}
+                {posts.map((post, index) => {
+                    const nodeRef = createRef();
+                    return (
+                        <CSSTransition
                             key={post.id}
-                            index={index + 1}
-                            post={post}
-                        />
-                    </CSSTransition>
-                ))}
+                            nodeRef={nodeRef}
+                            timeout={500}
+                            classNames="post"
+                        >
+                            <Post
+                                remove={removePost}
+                                key={post.id}
+                                index={index + 1}
+                                post={post}
+                            />
+                        </CSSTransition>
+                    )
+                })}
             </TransitionGroup>
         </div>
     );
