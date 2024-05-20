03238668fdcd03f03e797596afb7d12e8c5fddf6
Andrei Siarheyenka <84916548+AndrewSRGN@users.noreply.github.com>
2024-05-20 16:48:39 -0700

    Fix API PostService in PostService.js
    Throw Error when status code not equal 200

--- src/API/PostService.js
+++ src/API/PostService.js
@@ -1,15 +1,21 @@
 export default class PostService {
-    static _baseUrl = 'https://jsonplaceholder.typicode.com/posts';
+    static _baseUrl = 'https://jsonplaceholder.typicode.com/po22sts';
 
-    static async getAll(limit = 10, page = 1) {
+    static async getAll(limit = 100, page = 1) {
         const endpoint = `${this._baseUrl}?_limit=${limit}&_page=${page}`;
         const response = await fetch(endpoint);
+        if (response.status !== 200) {
+            throw new Error(`Failed to fetch posts. Status code: ${response.status}`);
+        }
         return await response.json();
     }
 
-    static async getPostById(id) {
+    static async getById(id) {
         const endpoint = `${this._baseUrl}/${id}`;
         const response = await fetch(endpoint);
+        if (response.status !== 200) {
+            throw new Error(`Failed to fetch post. Status code: ${response.status}`);
+        }
         return await response.json();
     }
 }
