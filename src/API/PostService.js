3e11628eed40ac7d7663d3b3b8924bbb6faa52ed
Andrei Siarheyenka <84916548+AndrewSRGN@users.noreply.github.com>
2024-05-20 18:40:13 -0700

    Refactor PostService API: PostService.js
    Method getAll() return Object {data[], totalCount} instead return data[]

--- src/API/PostService.js
+++ src/API/PostService.js
@@ -1,13 +1,17 @@
 export default class PostService {
     static _baseUrl = 'https://jsonplaceholder.typicode.com/posts';
 
-    static async getAll(limit = 100, page = 1) {
+    static async getAll(limit = 10, page = 1) {
         const endpoint = `${this._baseUrl}?_limit=${limit}&_page=${page}`;
         const response = await fetch(endpoint);
         if (response.status !== 200) {
             throw new Error(`Failed to fetch posts. Status code: ${response.status}`);
         }
-        return await response.json();
+        const data = await response.json();
+        return {
+            data: data,
+            totalCount: parseInt(response.headers.get('X-Total-Count'))
+        }
     }
 
     static async getById(id) {
