d8788167d6ff77de8955a0cb76204eeb7e6c8650
Andrei Siarheyenka <84916548+AndrewSRGN@users.noreply.github.com>
2024-05-21 21:35:51 -0700

    Improve useFetch hook: useFetch.js
    Now return Promise that resolves response or an array of responses.
    If an error occurs, it resolves to null

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,31 +1,44 @@
-import {useState} from "react";
+import {  useState } from "react";
 
 /**
  * Custom hook for executing asynchronous operations with loading and error state management.
  * It supports both single and multiple callback functions.
  *
- * @param {Function|Array<Function>} callbacks - A callback function or an array of callback functions,
- * each of which should be asynchronous and change the state.
- * @return {[fetchData:function, isLoading: boolean, error: string | null]} - An array containing the fetchData function, isLoading boolean, and error string or null.
+ * @param {Function|Array<Function>} callbacks - A callback function or an array of callback functions.
+ * @return {[fetchData: () => Promise<response | response[] | null>, isLoading: boolean, error: string | null]} - An array containing the fetchData function, isLoading boolean, and error string or null.
  */
 export const useFetch = (callbacks) => {
     const [isLoading, setIsLoading] = useState(true);
     const [error, setError] = useState(null);
 
+    /**
+     * Fetches data from the provided callback functions.
+     *
+     * @returns {PromiseLike<response | response[] | null>} - A promise that resolves to the response or an array of responses. If an error occurs, it resolves to null.
+     */
     const fetchData = async () => {
         setError(null);
         setIsLoading(true);
+
+        let callbackResponse = null;
+        
         try {
             if (Array.isArray(callbacks)) {
-                await Promise.all(callbacks.map(callback => callback()));
+                callbackResponse = [];
+                for (let i = 0; i < callbacks.length; i++) {
+                    const response = await callbacks[i]();
+                    callbackResponse.push(response);
+                }
             } else {
-                await callbacks();
+                callbackResponse = await callbacks();
             }
         } catch (error) {
             setError(error.message);
         } finally {
             setIsLoading(false);
         }
+
+        return callbackResponse;
     }
 
     return [fetchData, isLoading, error];
