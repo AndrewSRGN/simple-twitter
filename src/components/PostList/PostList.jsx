755afcfcdf4b62cce82a400847d57e8b32cd6a82
Andrei Siarheyenka <84916548+AndrewSRGN@users.noreply.github.com>
2024-05-22 16:57:01 -0700

    Fix animations when Post deleting: PostList.jsx

--- src/components/PostList/PostList.jsx
+++ src/components/PostList/PostList.jsx
@@ -1,14 +1,19 @@
-import React, {createRef} from 'react';
+import React, { createRef, useState } from "react";
 import Post from "../Post/Post";
 import './PostList.css';
 import {CSSTransition, TransitionGroup} from "react-transition-group";
 
 const PostList = ({title, posts, removePost}) => {
-
+    const [removingPostId, setRemovingPostId] = useState(null);
 
     if (!posts.length) {
         return <h1 style={{ textAlign: "center" }}>Posts not found</h1>;
     }
+
+    const handleRemovePost = (postId) => {
+        setRemovingPostId(postId);
+        removePost(postId);
+    };
 
     return (
         <div className="post-list">
@@ -16,16 +21,18 @@ const PostList = ({title, posts, removePost}) => {
             <TransitionGroup className="post-list__body">
                 {posts.map((post, index) => {
                     const nodeRef = createRef();
+                    const isRemoving = removingPostId === post.id;
                     return (
                         <CSSTransition
                             key={post.id}
                             nodeRef={nodeRef}
                             timeout={500}
                             classNames="post"
+                            in={!isRemoving}
                         >
                             <Post
                                 ref={nodeRef}
-                                remove={removePost}
+                                remove={() => handleRemovePost(post.id)}
                                 key={post.id}
                                 index={index + 1}
                                 post={post}
